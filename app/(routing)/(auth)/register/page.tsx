3e187aa3ab9de2b56cff1f5857ebea93924b517e
VidushaSanidu <vidushsanidu07@gmail.com>
2024-02-11 11:07:44 +0530

    fix bug

---
+++ app/(routing)/(auth)/register/page.tsx
@@ -0,0 +1,22 @@
+import RegisterPage from "@app/components/RegisterPage";
+import Link from "next/link";
+
+export default function LoginPage() {
+  return (
+    <div className="h-full">
+      <h1 className="mt-6 text-center font-cal text-3xl dark:text-white">
+        Welcome! please Sign-Up
+      </h1>
+      <RegisterPage />
+      <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+        or
+      </div>
+      <p className="text-center text-sm text-gray-600 mt-2">
+        If you have an account, please&nbsp;
+        <Link className="text-blue-500 hover:underline" href="/login">
+          Login
+        </Link>
+      </p>
+    </div>
+  );
+}
